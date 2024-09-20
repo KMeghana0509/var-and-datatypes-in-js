267acc844e564c238a7fd9a8facf0343aed945b0
KMeghana0509 <meghareddy.k002@gmail.com>
2024-09-20 13:31:55 +0530

    Added files

---
+++ Day1.js
@@ -0,0 +1,10 @@
+var name="Meghana";
+var age=22;
+var job="Developer";
+var sal=4.5;
+console.log(age);
+console.log(sal);
+console.log(job);
+console.log(name);
+var details=[1,"Meghana",22,"Developer",4.5,{city:"Nellore",pincode:524002,state:"Andhra pradesh"},["UNR","Narayana","Annamacharya"]]
+console.log(details);
